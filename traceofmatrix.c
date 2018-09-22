c1c07be0d9e8fffbdf34886accb5a0a6da4e91ae
aniketsg <43471607+aniketsg@users.noreply.github.com>
2018-09-22 22:26:12 +0530

    Add files via upload

---
+++ traceofmatrix.c
@@ -0,0 +1,27 @@
+//Trace of given matrix
+#include<stdio.h>
+void main()
+{
+int i,j,o,tr=0;
+printf("Enter order of matrix: \n");
+scanf("%d",&o);
+int a[o][o];
+printf("Enter the elements of matrix row-wise:\n");
+	for(i=0;i<o;i++)
+	{
+		for(j=0;j<o;j++)
+		{
+		scanf("%d",&a[i][j]);		
+		}
+	}
+
+	for(i=0;i<o;i++)
+	{
+		for(j=0;j<o;j++)
+		{
+		if(i==j)		//adding diagonal elements only
+		tr=tr+a[i][j];		
+		}
+	}
+printf("Trace of the given matrix is: %d",tr);
+}
