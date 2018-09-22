cf0718bc6381899d8a83a93a3f0dd553ba788eb8
aniketsg <43471607+aniketsg@users.noreply.github.com>
2018-09-22 09:00:58 +0530

    Update biggestnumberarray.c

--- biggestnumberarray.c
+++ biggestnumberarray.c
@@ -1,15 +1,16 @@
 //program to find biggest number in an array
+#define SIZE 5
 #include<stdio.h>
 int main()
 {
-int a[3],x,i,j;
-printf("Enter the three numbers:");
-for(i=0;i<=2;i++)
+int a[SIZE],x,i,j;
+printf("Enter the numbers:");
+for(i=0;i<=SIZE-1;i++)
 {
 scanf("%d",&a[i]);
 }
 x=a[0];
-for(j=1;j<=2;j++)
+for(j=1;j<=SIZE-1;j++)
 {
 if(x<a[j])
 x=a[j];
