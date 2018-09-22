902e92e9546bcbf4541802483706cc28038d3ef2
aniketsg <43471607+aniketsg@users.noreply.github.com>
2018-09-22 09:12:48 +0530

    Add files via upload

---
+++ charactervowel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+char ch;
+printf("Enter the character: ");
+scanf("%c",&ch);
+if(ch==97 || ch==101 || ch==105 || ch==111 || ch==117 || ch==65 || ch==69 || ch==73 ||ch==79 ||ch==85)
+printf("Given character is a vowel\n");
+else
+printf("Given character is not a vowel:\n");
+}
