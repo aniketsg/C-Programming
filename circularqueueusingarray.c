ee73a578dce688514491671e9e7c9e2e8d6db784
aniketsg <43471607+aniketsg@users.noreply.github.com>
2018-09-23 17:18:19 +0530

    Add files via upload

---
+++ circularqueueusingarray.c
@@ -0,0 +1,93 @@
+//Program to implement circular queue using array
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 5
+
+void push(int q[], int, int *, int *);
+void pop(int q[], int *, int *);
+void display(int q[]);
+
+void main()
+{
+	int i,ch,num;
+	int rear=-1, front=-1;
+	int q[SIZE]={0,0,0,0,0};
+
+	while(1)
+		{
+		printf("Enter choice:\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+		scanf("%d", &ch);
+		switch(ch)
+			{
+				case 1:
+					printf("Enter number to push on stack:\n");
+					scanf("%d", &num);
+					push(q, num, &front, &rear);
+					break;
+				case 2:
+					pop(q, &front, &rear);
+					break;
+				case 3:
+					display(q);
+					break;
+				case 4:
+				       exit(1);
+			       	default:
+					printf("Invalid choice entered! Please try again!\n");
+			 		break;		
+			}
+		}
+}
+
+void push(int q[], int num, int *f, int *r)
+{
+	if((*r==SIZE-1) && (*f==0) || ((*r)+1==(*f)))
+	{
+		printf("Queue is full!");
+		return;
+	}
+	else{
+		if(*r==SIZE-1)
+			{
+				*r=0;
+			}
+		    else{
+			(*r)++;
+			}
+		q[*r]=num;
+		if(*f==-1)		//when queue is empty, make front pointer point to first element
+		{
+			*f=0;
+		}
+       	    }
+}
+
+void pop(int q[], int *f, int *r)
+{
+	if(*f==-1)
+		{
+		printf("Queue is empty!\n");
+		return;
+		}
+	int temp=q[(*f)];
+	printf("--->%d<---\n", temp);
+	q[(*f)]=0;
+	if(*r==*f)
+		{
+		*r=-1;
+		*f=-1;
+		}
+	else{
+		(*f)++;
+            }
+}
+
+void display(int q[])
+{
+	int i;
+	for(i=0;i<SIZE;i++)
+		{
+		printf("%d\t", q[i]);
+		}
+	printf("\n");
+}
